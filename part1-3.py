ad228d6b7d824b549b9f2c2d143a6e0c6424ed87
lennymuldoon <lenny.muldoon@gmail.com>
2023-12-02 18:42:10 -0600

    created probabilityOfDiceNoReroll function

--- part1-3.py
+++ part1-3.py
@@ -2,3 +2,17 @@
 Name: Leonard (Lenny) Muldoon
 Date: 12/2/2023
 """
+
+import math
+
+numOfDice = 5
+
+#calculates the probability of rolling the same number on rolling a number of 6 sided dice (in this case 5)
+def probabilityOfDiceNoReroll(amountOfDice):
+    return ((1/6) ** amountOfDice) * 6
+
+
+
+
+
+print("The probability of rolling the same number on ", numOfDice, " dice is: ", probabilityOfDiceNoReroll(numOfDice))
