5ca4a2f5f916ba19056bf7522b5aaa9e1efefd6c
lennymuldoon <lenny.muldoon@gmail.com>
2023-12-02 18:45:12 -0600

    added ability to calculate with different sides

--- part1-3.py
+++ part1-3.py
@@ -6,13 +6,14 @@ Date: 12/2/2023
 import math
 
 numOfDice = 5
+sidesOfDice = 6
 
-#calculates the probability of rolling the same number on rolling a number of 6 sided dice (in this case 5)
-def probabilityOfDiceNoReroll(amountOfDice):
-    return ((1/6) ** amountOfDice) * 6
-
-
-
+#calculates the probability of rolling the same number on rolling a number of sided dice (in this case 5 6 sided dice)
+def probabilityOfDiceNoReroll(amountOfDice, sides):
+    return ((1/sides) ** amountOfDice) * sides
 
 
-print("The probability of rolling the same number on ", numOfDice, " dice is: ", probabilityOfDiceNoReroll(numOfDice))
+
+
+
+print("The probability of rolling the same number on ", numOfDice, " ", sidesOfDice, " sided dice is: ", probabilityOfDiceNoReroll(numOfDice, sidesOfDice))
