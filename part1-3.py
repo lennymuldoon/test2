16e91adc1ef01337774cd72a09812855f335375b
lennymuldoon <lenny.muldoon@gmail.com>
2023-12-04 10:29:00 -0600

    added rollDice function to roll die

--- part1-3.py
+++ part1-3.py
@@ -4,15 +4,23 @@ Date: 12/2/2023
 """
 
 import math
+import random
 
 numOfDice = 5
 sidesOfDice = 6
+timesAllFive = 0
 
 #calculates the probability of rolling the same number on rolling a number of sided dice (in this case 5 6 sided dice) without rerolling
 def probabilityOfDiceNoReroll(amountOfDice, sides):
     return ((1/sides) ** amountOfDice) * sides
 
+def rollDice(amountOfDice):
+    return [random.randint(1, 6) for i in range(amountOfDice)]
 
+
+dice = rollDice(numOfDice)
+print(dice)
+    
 
 
 
