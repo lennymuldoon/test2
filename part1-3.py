25f62e779af6e830e5b82a4e247bd2145fd4c6b9
lennymuldoon <lenny.muldoon@gmail.com>
2023-12-04 14:25:42 -0600

    created findHighestCount function

--- part1-3.py
+++ part1-3.py
@@ -18,10 +18,25 @@ def rollDice(amountOfDice, sides):
 def allSame(diceRolls):
     return all(d == diceRolls[0] for d in diceRolls)
 
+def findHighestCount(diceRolls):
+    num = 0
+    count = 0
+    highestCount = 0
+    for i in range(len(diceRolls)):
+        count = 0
+        num = diceRolls[0]
+        for d in diceRolls:
+            if diceRolls[i] == num:
+                count += 1
+        if count > highestCount:
+            highestCount = num
+    return num
+
 def main():
     numOfDice = 5
     sidesOfDice = 6
     timesAllFive = 0
+    #part 1 answers
     print("The probability of rolling the same number on", numOfDice, sidesOfDice, "sided dice is:", probabilityOfDiceNoReroll(numOfDice, sidesOfDice))
     dice = rollDice(numOfDice, sidesOfDice)
     for i in range(100000):
@@ -30,6 +45,12 @@ def main():
         dice = rollDice(numOfDice, sidesOfDice)
 
     print("Number of times out of 100000 rolls that dice were the same:", timesAllFive)
+    print(dice)
+    print(findHighestCount(dice))
+
+    #part 2 answers
+    #for d in dice:
+
 
 
 main()
